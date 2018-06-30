47e11ee9774964db7f98f59929cdd226b520c0cb
dmp <doug@purdy.im>
2018-06-30 02:35:53 -0700

    complications 2

--- Zendo WatchKit Extension/ZendoDataSource.swift
+++ Zendo WatchKit Extension/ZendoDataSource.swift
@@ -7,9 +7,12 @@
 //
 
 import WatchKit
+import HealthKit
 
 class ZendoDataSource: NSObject, CLKComplicationDataSource
 {
+    var currentHrv : Double = 0.0
+    
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void)
     {
         handler([])
@@ -17,45 +20,145 @@ class ZendoDataSource: NSObject, CLKComplicationDataSource
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void)
     {
-        if complication.family == .modularSmall {
-            
-            let template = CLKComplicationTemplateModularSmallStackImage()
-            template.line1ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "shobogenzo")!)
-            template.line2TextProvider = CLKSimpleTextProvider(text: "66")
-            template.tintColor = .white
-            template.highlightLine2 = true
         
-            let entry = CLKComplicationTimelineEntry(date: Date(),
-                                                 complicationTemplate: template)
-            handler(entry)
-        }
-        else
+        
+        let entry = CLKComplicationTimelineEntry(date: Date(),
+                                                 complicationTemplate: getTemplate(complication: complication))
+        handler(entry)
+    }
+    
+    func getHrv() -> String {
+        
+        let hkType  = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRateVariabilitySDNN)!
+        
+        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())
+        
+        let hkPredicate = HKQuery.predicateForSamples(withStart: yesterday, end: Date(), options: .strictEndDate)
+        
+        let options : HKStatisticsOptions  = HKStatisticsOptions.discreteAverage
+        
+        let hkQuery = HKStatisticsQuery(quantityType: hkType,
+                                        quantitySamplePredicate: hkPredicate,
+                                        options: options)
         {
-            handler(nil)
-        }
-    }
-    
-    
-    func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
-        handler(.showOnLockScreen)
-    }
-    
-    func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        
-        
-        if complication.family == .modularSmall {
+            query, result, error in
             
-            let template = CLKComplicationTemplateModularSmallStackImage()
-            template.line1ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "shobogenzo")!)
-            template.line2TextProvider = CLKSimpleTextProvider(text: "66")
-            template.tintColor = .white
-            template.highlightLine2 = true
-
-        handler(template)
-        } else {
-            handler(nil)
+            if(error != nil)
+            {
+                print(error.debugDescription)
+                
+            }
+            else
+            {
+                if let value = result!.averageQuantity()?.doubleValue(
+                    for: HKUnit(from: "ms"))
+                {
+                    DispatchQueue.main.async()
+                        {
+                            if value > 0.0
+                            {
+                                self.currentHrv = value
+                            }
+                    }
+                }
+            }
         }
+        
+        ZBFHealthKit.healthStore.execute(hkQuery)
+        
+        return Int(self.currentHrv).description
+        
     }
     
+    
+    func getTemplate (complication: CLKComplication) -> CLKComplicationTemplate
+    {
+        var retval : CLKComplicationTemplate? = nil
+        
+        switch complication.family
+        {
+       
+        case .modularSmall:
+            
+            let template = CLKComplicationTemplateModularSmallSimpleText()
+            template.textProvider = CLKSimpleTextProvider(text: getHrv())
+            template.tintColor = .white
+            
+            retval = template
+            
+            break
+            
+        case .modularLarge:
+            
+            let template = CLKComplicationTemplateModularLargeTallBody()
+            template.headerTextProvider = CLKSimpleTextProvider(text: "Zendo")
+            template.bodyTextProvider = CLKSimpleTextProvider(text: getHrv())
+            template.tintColor = .white
+            
+            retval = template
+            
+            break
+            
+        case .circularSmall:
+            
+            let template = CLKComplicationTemplateCircularSmallSimpleText()
+            template.textProvider = CLKSimpleTextProvider(text: getHrv())
+            template.tintColor = .white
+            retval = template
+            
+            break
+            
+        case .extraLarge:
+            
+            let template = CLKComplicationTemplateExtraLargeSimpleText()
+            template.textProvider = CLKSimpleTextProvider(text: getHrv())
+            template.tintColor = .white
+            retval = template
+            
+            break
+            
+        case .utilitarianLarge:
+            
+            let template = CLKComplicationTemplateUtilitarianLargeFlat()
+            template.tintColor = .white
+            retval = template
+            
+            break
+            
+        case .utilitarianSmall:
+            
+            let template = CLKComplicationTemplateUtilitarianSmallSquare()
+            template.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "Utilitarian")!)
+            template.tintColor = .white
+            retval = template
+            
+            break
+            
+        case .utilitarianSmallFlat:
+            
+            let template = CLKComplicationTemplateUtilitarianSmallFlat()
+            template.textProvider = CLKSimpleTextProvider(text: getHrv())
+            template.tintColor = .white
+            retval = template
+            
+            break
+            
+        default:
+            
+            break
+            
+        }
+    
+        return retval!
+    }
+    
+        
+        func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
+            handler(.showOnLockScreen)
+        }
+        
+        func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
+            
+            handler(getTemplate(complication: complication))
+        }
 }
-
