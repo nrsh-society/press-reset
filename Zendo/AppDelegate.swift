fe00d1789cf15d514913f5e03f5b50f6a78a0b07
Billy Yuen <Billy_Yuen@intuit.com>
2019-05-30 10:44:06 -0700

    download optIn json and match function for email address.   Add persisted property for last uploaded session

--- Zendo/AppDelegate.swift
+++ Zendo/AppDelegate.swift
@@ -49,6 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             Settings.startTrialDateStr = Date().toUTCSubscriptionString
         }
         
+        OptIn.load()
         return true
     }
     
@@ -63,7 +64,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-         //#todo
+            //#todo
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         
@@ -72,6 +73,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillEnterForeground(_ application: UIApplication) {
          //#todo
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("activate")
+ 
+        OptIn.uploadSessions()
         
     }
     
