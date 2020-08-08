ebfda27cf680cbc7bf5c637d6e3489e878a1dfef
dpurdy <dpurdy@purdy.im>
2020-08-07 20:40:59 -0700

    init

--- Zendo/Controllers/Discover/LabController.swift
+++ Zendo/Controllers/Discover/LabController.swift
@@ -16,24 +16,30 @@ import AVKit
 import Mixpanel
 import Cache
 import SwiftyJSON
-import WebKit
-import SceneKit
+import Vision
+import HaishinKit
 
-class LabController: UIViewController
+class LabController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate
 {
-    
-    static func loadFromStoryboard() -> LabController
-    {
-        let controller = UIStoryboard(name: "LabController", bundle: nil).instantiateViewController(withIdentifier: "LabController") as! LabController
-        
-        return controller
-    }
-    
     var appleWatch : Zensor?
     var story: Story!
     var idHero = ""
     var panGR: UIPanGestureRecognizer!
     var chartHR = [String: Int]()
+    let player = SKSpriteNode(imageNamed: "player1")
+    var ring: Int = 0
+    var showLevels : Bool = false
+    var showGroups : Bool = false
+    var rings = [SKShapeNode]()
+    var scene: SKScene
+    
+    private let captureSession = AVCaptureSession()
+    private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+    private var drawings: [CAShapeLayer] = []
+    
+    private let rtmpConnection = RTMPConnection()
+    private lazy var rtmpStream = RTMPStream(connection: self.rtmpConnection)
     
     @IBOutlet weak var sceneView: SCNView!
     {
@@ -67,8 +73,132 @@ class LabController: UIViewController
             
         }
     }
+
+    
+    static func loadFromStoryboard() -> LabController
+    {
+        let controller = UIStoryboard(name: "LabController", bundle: nil).instantiateViewController(withIdentifier: "LabController") as! LabController
+        
+        return controller
+    }
     
    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.previewLayer.frame = self.view.frame
+    }
+    
+    func captureOutput(
+        _ output: AVCaptureOutput,
+        didOutput sampleBuffer: CMSampleBuffer,
+        from connection: AVCaptureConnection) {
+        
+        guard let frame = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            debugPrint("unable to get image from sample buffer")
+            return
+        }
+        self.detectFace(in: frame)
+    }
+    
+    private func addCameraInput() {
+        guard let device = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [.builtInWideAngleCamera, .builtInDualCamera, .builtInTrueDepthCamera],
+            mediaType: .video,
+            position: .front).devices.first else {
+                fatalError("No back camera device found, please make sure to run SimpleLaneDetection in an iOS device and not a simulator")
+        }
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
+    private func showCameraFeed() {
+        self.previewLayer.videoGravity = .resizeAspectFill
+        self.view.layer.addSublayer(self.previewLayer)
+        self.previewLayer.zPosition = -1
+        self.previewLayer.frame = self.view.frame
+    }
+    
+    private func getCameraFrames() {
+        self.videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        self.videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        self.videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+        self.captureSession.addOutput(self.videoDataOutput)
+        guard let connection = self.videoDataOutput.connection(with: AVMediaType.video),
+            connection.isVideoOrientationSupported else { return }
+        connection.videoOrientation = .portrait
+    }
+    
+    private func detectFace(in image: CVPixelBuffer) {
+        let faceDetectionRequest = VNDetectFaceLandmarksRequest(completionHandler: { (request: VNRequest, error: Error?) in
+            DispatchQueue.main.async {
+                if let results = request.results as? [VNFaceObservation] {
+                    self.handleFaceDetectionResults(results)
+                } else {
+                    self.clearDrawings()
+                }
+            }
+        })
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: image, orientation: .leftMirrored, options: [:])
+        try? imageRequestHandler.perform([faceDetectionRequest])
+    }
+    
+    private func handleFaceDetectionResults(_ observedFaces: [VNFaceObservation]) {
+        
+        self.clearDrawings()
+        let facesBoundingBoxes: [CAShapeLayer] = observedFaces.flatMap({ (observedFace: VNFaceObservation) -> [CAShapeLayer] in
+            let faceBoundingBoxOnScreen = self.previewLayer.layerRectConverted(fromMetadataOutputRect: observedFace.boundingBox)
+            let faceBoundingBoxPath = CGPath(rect: faceBoundingBoxOnScreen, transform: nil)
+            let faceBoundingBoxShape = CAShapeLayer()
+            faceBoundingBoxShape.path = faceBoundingBoxPath
+            faceBoundingBoxShape.fillColor = UIColor.clear.cgColor
+            faceBoundingBoxShape.strokeColor = UIColor.green.cgColor
+            var newDrawings = [CAShapeLayer]()
+            newDrawings.append(faceBoundingBoxShape)
+            if let landmarks = observedFace.landmarks {
+                newDrawings = newDrawings + self.drawFaceFeatures(landmarks, screenBoundingBox: faceBoundingBoxOnScreen)
+            }
+            return newDrawings
+        })
+        facesBoundingBoxes.forEach({ faceBoundingBox in self.view.layer.addSublayer(faceBoundingBox) })
+        self.drawings = facesBoundingBoxes
+    }
+    
+    private func clearDrawings() {
+        self.drawings.forEach({ drawing in drawing.removeFromSuperlayer() })
+    }
+    
+    private func drawFaceFeatures(_ landmarks: VNFaceLandmarks2D, screenBoundingBox: CGRect) -> [CAShapeLayer] {
+        var faceFeaturesDrawings: [CAShapeLayer] = []
+        if let leftEye = landmarks.leftEye {
+            let eyeDrawing = self.drawEye(leftEye, screenBoundingBox: screenBoundingBox)
+            faceFeaturesDrawings.append(eyeDrawing)
+        }
+        if let rightEye = landmarks.rightEye {
+            let eyeDrawing = self.drawEye(rightEye, screenBoundingBox: screenBoundingBox)
+            faceFeaturesDrawings.append(eyeDrawing)
+        }
+        // draw other face features here
+        return faceFeaturesDrawings
+    }
+    private func drawEye(_ eye: VNFaceLandmarkRegion2D, screenBoundingBox: CGRect) -> CAShapeLayer {
+        let eyePath = CGMutablePath()
+        let eyePathPoints = eye.normalizedPoints
+            .map({ eyePoint in
+                CGPoint(
+                    x: eyePoint.y * screenBoundingBox.height + screenBoundingBox.origin.x,
+                    y: eyePoint.x * screenBoundingBox.width + screenBoundingBox.origin.y)
+            })
+        eyePath.addLines(between: eyePathPoints)
+        eyePath.closeSubpath()
+        let eyeDrawing = CAShapeLayer()
+        eyeDrawing.path = eyePath
+        eyeDrawing.fillColor = UIColor.clear.cgColor
+        eyeDrawing.strokeColor = UIColor.green.cgColor
+        
+        return eyeDrawing
+    }
+    
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -89,10 +219,29 @@ class LabController: UIViewController
         panGR = UIPanGestureRecognizer(target: self, action: #selector(pan))
         
         sceneView.addGestureRecognizer(panGR)
+        sceneView.backgroundColor = .clear
+        
+        self.scene = self.setupScene()
+        
         
         setupConnectButton()
         
         self.startSession()
+        
+        self.addCameraInput()
+        self.showCameraFeed()
+        self.getCameraFrames()
+        self.captureSession.startRunning()
+        
+        rtmpStream.attachAudio(AVCaptureDevice.default(for: AVMediaType.audio)) { error in
+              // print(error)
+          }
+          rtmpStream.attachCamera(DeviceUtil.device(withPosition: .front)) { error in
+              // print(error)
+          }
+          
+        rtmpConnection.connect("rtmp://live-sjc05.twitch.tv/app/live_526664141_tw0025TCdNZBqEkTxmhAllcIQVlvfQ")
+        rtmpStream.publish("streamName")
 
     }
     
@@ -177,6 +326,7 @@ class LabController: UIViewController
                 UIView.animate(withDuration: 0.5)
                 {
                     self.arenaView.isHidden = false
+                    self.progressView.isHidden = false
                     self.connectButton.isHidden = true
                 }
 
@@ -196,7 +346,7 @@ class LabController: UIViewController
                     
                     self.arenaView.isHidden = true
                     self.connectButton.isHidden = false
-                    
+                    self.progressView.isHidden = true
                     self.arenaView.hrv.text = "--"
                     self.arenaView.time.text = "--"
                     self.arenaView.setChart([])
@@ -218,6 +368,147 @@ class LabController: UIViewController
             }
         }
     }
+    
+    func addShell(_ parent: SKNode, _ radius: Int, _ name: String)
+    {
+  
+        let pathNode = SKShapeNode(circleOfRadius: CGFloat(radius))
+        
+        pathNode.strokeColor = SKColor.zenWhite
+        
+        pathNode.position = CGPoint(x: parent.frame.midX, y: parent.frame.midY)
+        
+        pathNode.fillColor = SKColor(red:0.06, green:0.15, blue:0.13, alpha:0.3)
+       
+        pathNode.zPosition = 3
+        
+        pathNode.name = name
+        
+        pathNode.isHidden = true
+        
+        pathNode.lineWidth = CGFloat(0.01)
+        
+        parent.addChild(pathNode)
+        
+        rings.append(pathNode)
+    }
+    
+    func startBackgroundContent(story : Story, completion: @escaping (AVPlayerItem) -> Void)
+    {
+        var playerItem: AVPlayerItem?
+        
+        let streamString = story.content[0].stream
+        let downloadString = story.content[0].download
+        
+        var downloadUrl : URL?
+        var streamUrl : URL?
+        
+        if let urlString = downloadString, let url = URL(string: urlString)
+        {
+            downloadUrl = url
+        }
+        
+        if let urlString = streamString, let url = URL(string: urlString)
+        {
+            streamUrl = url
+        }
+        
+        storage?.async.entry(forKey: downloadUrl?.absoluteString ?? "", completion:
+        {
+            result in
+            
+            switch result
+            {
+                case .value(let entry):
+                
+                    if var path = entry.filePath
+                    {
+                        if path.first == "/"
+                        {
+                            path.removeFirst()
+                        }
+                    
+                        let url = URL(fileURLWithPath: path)
+                    
+                        playerItem = AVPlayerItem(url: url)
+                    }
+            
+                default:
+                    
+                    if let url = streamUrl
+                    {
+                        playerItem = AVPlayerItem(url: url)
+                    }
+                    else
+                    {
+                        playerItem = AVPlayerItem(url: downloadUrl!)
+                    }
+            }
+            
+            completion(playerItem!)
+            
+        })
+    }
+    
+    func setupScene() -> SCNScene
+    {
+        sceneView.frame = UIScreen.main.bounds
+        sceneView.contentMode = .scaleAspectFill
+        
+        let scene = SCNScene()
+        
+        
+        sceneView.backgroundColor = .clear
+        
+        self.startBackgroundContent(story: story, completion:
+        {
+            item in
+            
+            DispatchQueue.main.async
+            {
+                let videoPlayer = AVPlayer(playerItem: item)
+                
+                let video = SKVideoNode(avPlayer: videoPlayer)
+                    
+                video.zPosition = 1.0
+                video.size = scene.frame.size
+                video.position = scene.position
+                video.anchorPoint = scene.anchorPoint
+                video.play()
+                scene.
+                scene.addChild(video)
+                    
+                NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime,
+                                                           object: videoPlayer.currentItem, queue: nil)
+                    {
+                       notification in
+                        
+                        DispatchQueue.main.async
+                        {
+                            videoPlayer.seek(to: kCMTimeZero)
+                            videoPlayer.play()
+                        }
+                        
+                    }
+                }
+            
+        })
+    
+        self.addShell(scene, 30, "2")
+        self.addShell(scene, 90, "1")
+        self.addShell(scene, 150, "0")
+        
+        return scene
+        
+    }
+    
+    let diskConfig = DiskConfig(name: "DiskCache")
+    let memoryConfig = MemoryConfig(expiry: .never, countLimit: 10, totalCostLimit: 10)
+    
+    lazy var storage: Cache.Storage? = {
+        return try? Cache.Storage(diskConfig: diskConfig, memoryConfig: memoryConfig, transformer: TransformerFactory.forData())
+    }()
+  
     
     @objc func sample(notification: NSNotification)
     {
@@ -245,6 +536,14 @@ class LabController: UIViewController
 
                 self.arenaView.setChart(chartHR)
     
+            }
+            
+            
+            DispatchQueue.main.async
+            {
+                
+                self.progressView.hrv.text = "1" //
+                self.progressView.time.text = "1"
             }
             
             if let watch  = self.appleWatch
--- Zendo/View/StartSession/ProgressView/ProgressView.swift
+++ Zendo/View/StartSession/ProgressView/ProgressView.swift
@@ -12,7 +12,6 @@ import Charts
 class ProgressView: UIView {
     
     @IBOutlet weak var hudView: UIView!
-    @IBOutlet weak var connectButton: UIButton!
     @IBOutlet weak var hrv: UILabel!
     @IBOutlet weak var time: UILabel!
     @IBOutlet weak var hrvImage: UIImageView! {
@@ -31,69 +30,7 @@ class ProgressView: UIView {
     
     @IBOutlet weak var hrvLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
-    
-    @IBOutlet weak var lineChartView: LineChartView! {
-        didSet {
-            
-            lineChartView.clipsToBounds = true
-            lineChartView.layer.cornerRadius = 20.0
-            
-            let zendoFont = UIFont.zendo(font: .antennaRegular, size: 10.0)
-            
-            lineChartView.noDataText = ""
-            lineChartView.autoScaleMinMaxEnabled = true
-            lineChartView.chartDescription?.enabled = false
-            lineChartView.drawGridBackgroundEnabled = false
-            lineChartView.pinchZoomEnabled = false
-            
-            let xAxis = lineChartView.xAxis
-            xAxis.drawGridLinesEnabled = false
-            xAxis.drawAxisLineEnabled = false
-            xAxis.labelPosition = .bottom
-            xAxis.labelTextColor = UIColor.zenGray
-            xAxis.labelFont = zendoFont
-            
-            let rightAxis = lineChartView.rightAxis
-            rightAxis.drawAxisLineEnabled = false
-            rightAxis.drawGridLinesEnabled = false
-            rightAxis.labelTextColor = UIColor.white
-            rightAxis.labelPosition = .insideChart
-            rightAxis.labelFont = zendoFont
-            rightAxis.yOffset = -12.0
-            rightAxis.drawTopYLabelEntryEnabled = false
-            rightAxis.labelCount = 5
-            
-            let leftAxis = lineChartView.leftAxis
-            leftAxis.drawAxisLineEnabled = false
-            leftAxis.drawGridLinesEnabled = false
-            leftAxis.drawLabelsEnabled = false
-            
-            //lineChartView.setViewPortOffsets(left: 0, top: 0, right: 0, bottom: 0)
-            lineChartView.highlightPerTapEnabled = false
-            lineChartView.highlightPerDragEnabled = false
-            lineChartView.doubleTapToZoomEnabled = false
-            
-            lineChartView.legend.enabled = false
-            
-            lineChartView.highlightPerTapEnabled = false
-            
-            lineChartView.highlightValues([])
-            lineChartView.xAxis.drawGridLinesEnabled = false
-            lineChartView.xAxis.drawAxisLineEnabled = false
-            lineChartView.rightAxis.drawAxisLineEnabled = false
-            lineChartView.leftAxis.drawAxisLineEnabled = false
-            
-            lineChartView.drawGridBackgroundEnabled = false
-            lineChartView.chartDescription?.enabled = false
-            lineChartView.autoScaleMinMaxEnabled = true
-            lineChartView.noDataText = ""
-            
-            initChartData()
-            setChart([])
-        }
-    }
-    
-    var chartData: LineChartData? = nil
+        
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -107,54 +44,6 @@ class ProgressView: UIView {
         loadNib()
     }
     
-    func initChartData() {
-        let dataset = LineChartDataSet(entries: [ChartDataEntry](), label: "")
-        dataset.mode = .horizontalBezier
-        dataset.drawValuesEnabled = false
-        dataset.setColor(UIColor.white)
-        dataset.lineWidth = 1.5
-        dataset.label = "bpm"
-        dataset.fillAlpha = 0.3
-        dataset.fillColor = UIColor.white
-        dataset.drawFilledEnabled = true
-        
-        dataset.drawCirclesEnabled = false
-        
-        self.chartData = LineChartData(dataSets: [dataset])
-    }
-    
-    func setChart(_ hrv: [(key: String, value: Int)]) {
-        
-        let dataset = chartData?.getDataSetByIndex(0)!
-        
-        dataset?.clear()
-        
-        if lineChartView.data == nil {
-            lineChartView.data = chartData
-        }
-        
-        for entry in hrv {
-            if entry.value > Int(0.0) {
-                if let key = Double(entry.key) {
-                    _ = dataset?.addEntry(ChartDataEntry(x: key, y: Double(entry.value)))
-                }
-            }
-        }
-        
-        let formato = MMChartHRVFormatter()
-        
-        let xaxis = XAxis()
-        xaxis.valueFormatter = formato
-        
-        
-        lineChartView.xAxis.valueFormatter = xaxis.valueFormatter
-        lineChartView.xAxis.avoidFirstLastClippingEnabled = true
-        lineChartView.xAxis.axisMaxLabels = 5
-        self.chartData?.notifyDataChanged()
-        lineChartView.notifyDataSetChanged()
-        lineChartView.fitScreen()
-        
-    }
     
     override func layoutSubviews()
     {
